0ebc641c2a9630e36c473982930a3a6621b5e8e1
johnsalzy <salzyjohn@gmail.com>
2019-03-06 22:58:21 -0500

    added venv, test script working

---
+++ test.py
@@ -0,0 +1,33 @@
+##This example was taken from https://sandbox.coinmarketcap.com/api/v1/#section/Quick-Start-Guide
+##Just a test to see if key works!
+
+from requests import Request, Session
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+import json
+
+#Get key from .env file
+import setup
+
+api_key = setup.api_key
+
+url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+parameters = {
+    'start': '1',
+    'limit': '5000',
+    'convert': 'USD',
+}
+headers = {
+    'Accepts': 'application/json',
+    'X-CMC_PRO_API_KEY': api_key,
+}
+
+session = Session()
+session.headers.update(headers)
+
+try:
+    response = session.get(url, params=parameters)
+    data = json.loads(response.text)
+    print(data)
+except (ConnectionError, Timeout, TooManyRedirects) as e:
+    print(e)
+  
