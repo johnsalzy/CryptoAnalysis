53fae15007a29ef275a8252787042ff2c276f01f
johnsalzy <salzyjohn@gmail.com>
2019-03-09 14:56:13 -0500

    updated requirements

--- test.py
+++ test.py
@@ -1,33 +1,22 @@
-##This example was taken from https://sandbox.coinmarketcap.com/api/v1/#section/Quick-Start-Guide
 ##Just a test to see if key works!
-
-from requests import Request, Session
-from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
-import json
-
+##Also see if database connection works.
+from collectCoinData import getLatestCoinData
+from updateDatabase import test_graph_connection
 #Get key from .env file
 import setup
-
 api_key = setup.api_key
 
-url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
-parameters = {
-    'start': '1',
-    'limit': '100',
-    'convert': 'USD',
-}
-headers = {
-    'Accepts': 'application/json',
-    'X-CMC_PRO_API_KEY': api_key,
-}
 
-session = Session()
-session.headers.update(headers)
+def main():
+    url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+    coinCount = '100'
+    conversion = 'USD'
+    coinData = getLatestCoinData(api_key, url, coinCount, conversion)
+    print('If connection to coinmarketcap api works,',
+          'you will see data below: \n', coinData)
+    test_graph_connection()
 
-try:
-    response = session.get(url, params=parameters)
-    data = json.loads(response.text)
-    print(data)
-except (ConnectionError, Timeout, TooManyRedirects) as e:
-    print(e)
+
+if __name__ == "__main__":
+    main()
   
